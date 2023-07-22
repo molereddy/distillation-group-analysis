672c2843cfc008910a3cd49ca51db1f183f1291f
molereddy <m.anmolreddy@gmail.com>
2023-07-22 15:57:34 +0000

    bug fix

--- loss.py
+++ loss.py
@@ -57,9 +57,9 @@ class LossComputer:
     
     def loss_mse(self, yhat, y, tft, sft, group_idx=None, is_training=False):
         # compute per-sample and per-group losses
-        mse_loss = torch.mean((sft - tft) ** 2, dim=(1, 2, 3))
-        group_loss, group_count = self.compute_group_avg(mse_loss, group_idx)
-        group_acc, group_count = self.compute_group_avg((torch.argmax(yhat,1) == y).float(), group_idx)
+        per_sample_losses = torch.mean((sft - tft) ** 2, dim=(1, 2, 3))
+        group_loss, group_count = self.compute_group_avg(per_sample_losses, group_idx)
+        group_acc, group_count = self.compute_group_avg((torch.argmax(yhat, 1) == y).float(), group_idx)
         # compute overall loss
         actual_loss = per_sample_losses.mean()
         weights = None

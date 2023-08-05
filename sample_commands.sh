493e23dec6652b05973b0f775f14f91a7ce7dce8
molereddy <m.anmolreddy@gmail.com>
2023-08-05 16:32:54 +0000

    sample dro command with our method

--- sample_commands.sh
+++ sample_commands.sh
@@ -18,3 +18,5 @@ python3 save_datasets.py -s confounder -d jigsaw -t toxicity -c identity_any --m
 python3 run_exp.py --method ERM -s confounder -d MultiNLI -t gold_label_random -c sentence2_has_negation --model bert --batch_size 32 
 
 python3 run_exp.py --method ERM -s confounder -d jigsaw -t toxicity -c identity_any --model bert-base-uncased --batch_size 24
+
+TZ=IST-5:30 date; python3 run_exp.py --method aux_wt -s confounder -d CUB -t waterbird_complete95 -c forest2water2 --model resnet18-pt --teacher resnet50-pt_group_DRO --batch_size 64 --alpha 6 --beta 3 --n_epochs 20; TZ=IST-5:30 date; 
